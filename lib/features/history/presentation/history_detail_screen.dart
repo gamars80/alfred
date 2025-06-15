e8ee9bcb977175544f3a95bd728bf523e1d609d4
gamars80 <gamars80@gmail.com>
2025-06-15 19:10:05 +0900

    fix

--- lib/features/history/presentation/history_detail_screen.dart
+++ lib/features/history/presentation/history_detail_screen.dart
@@ -209,7 +209,7 @@ class _HistoryDetailScreenState extends State<HistoryDetailScreen> {
                                     }(),
                                     mainAxisSpacing: 12,
                                     crossAxisSpacing: 12,
-                                      mainAxisExtent: 280
+                                      mainAxisExtent: 290
                                   ),
                                   itemCount: products.length,
                                   itemBuilder: (context, index) {
